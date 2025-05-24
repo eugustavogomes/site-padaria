13ffa30f9a99eb3e3c009f5d84817e1a72276adc
Gustavo Gomes <lgglima@gmail.com>
2025-05-24 19:34:42 -0300

    centralizando o botão de contato

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -3,24 +3,26 @@ import Map from '../Map';
 
 const ContactSection = () => (
   <section id="contato" className="py-12 max-w-4xl mx-auto px-4">
-        <h2 className="text-3xl font-semibold text-beige-200 mb-4">Contato</h2>
-        <p className="text-beige-300 mb-2">Avenida Paraná, 3059 - loja 20 - Boa Vista, Curitiba, Brazil 82510-000</p>
-        <p className="text-beige-300 mb-2">Telefone: (41) 99575-1004</p>
+    <h2 className="text-3xl font-semibold text-beige-200 mb-4">Contato</h2>
+    <p className="text-beige-300 mb-2">Avenida Paraná, 3059 - loja 20 - Boa Vista, Curitiba, Brazil 82510-000</p>
+    <p className="text-beige-300 mb-2">Telefone: (41) 99575-1004</p>
 
-        {/* Mapa Integrado */}
-        <div className="my-6">
-          <Map />
-        </div>
+    {/* Mapa Integrado */}
+    <div className="my-6">
+      <Map />
+    </div>
 
-        <a
-          href="https://wa.me/41995751004"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-block bg-gold-500 text-dark-900 py-2 px-4 rounded hover:bg-gold-600"
-        >
-          Fale pelo WhatsApp
-        </a>
-      </section>
+    <div className="flex justify-center">
+      <a
+        href="https://wa.me/41995751004"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-gold-500 text-dark-900 py-2 px-4 rounded hover:bg-gold-600"
+      >
+        Fale pelo WhatsApp
+      </a>
+    </div>
+  </section>
 );
 
 export default ContactSection;
